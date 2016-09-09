61d6ed2ee1c2b1def2f484ab53e874fa14e51b89
jilong <skyujilong@163.com>
2016-09-09 19:20:50 +0800

    add change event support

--- pages/js/mods/ui/card-slider/index.js
+++ pages/js/mods/ui/card-slider/index.js
@@ -1,10 +1,16 @@
 "use strict";
 require('./scss/card-slider.scss');
 
-function CardSlider($el) {
+function CardSlider($el,opt) {
     var method, _self = this,
         _clzName = 'card-slider',
         itemWidth, itemSpace, activeNum;
+    opt = opt || {
+        bar:true
+    };
+    this.events = {
+        'change':[]
+    };
     this.$el = $el;
     this.$el.addClass(_clzName);
     this.$elLiList = $el.children('ul').children('li');
@@ -13,14 +19,20 @@ function CardSlider($el) {
         init: function() {
             this.initSpace();
             this.initPos(0);
+            if(opt && opt.bar){
+                this.initBar();
+            }
             this.bindEvent();
+
         },
         bindEvent: function() {
+            var tThis = this;
             _self.$elLiList.on('swipeLeft', function() {
                 if (activeNum == _self.$elLiList.length - 1) {
                     return;
                 }
                 activeNum++;
+                tThis.activeNumChange(activeNum);
                 $el.children('ul').css({
                     'transform': 'translateX(' + -itemSpace * (activeNum) + 'px)'
                 });
@@ -61,6 +73,7 @@ function CardSlider($el) {
                     return;
                 }
                 activeNum--;
+                tThis.activeNumChange(activeNum);
                 $el.children('ul').css({
                     'transform': 'translateX(' + -itemSpace * (activeNum) + 'px)'
                 });
@@ -119,15 +132,52 @@ function CardSlider($el) {
                 });
             });
         },
+        activeNumChange: function(num){
+            //TODO activeNumChange
+            if(opt.bar){
+                this.initBarPos();
+            }
+            this.noti('change',num);
+        },
         initSpace: function() {
             var width = _self.$el.width();
             var length = _self.$elLiList.length;
             itemSpace = Math.floor((width - itemWidth) / 2 / length);
+        },
+        initBar: function(){
+            this.$bar = $el.find('.card-scroll-bar');
+            this.$barBtn = $el.find('.card-bar');
+            this.$barWidth = this.$bar.width();
+            this.$barBtnWidth = this.$barWidth / _self.$elLiList.length;
+            this.$barBtn.css({
+                width:this.$barBtnWidth + 'px'
+            });
+            this.initBarPos();
+        },
+        initBarPos: function(){
+            this.$barBtn.css({
+                '-webkit-transform': 'translateX(' + this.$barBtnWidth * (activeNum) + 'px)',
+                'transform':'translateX(' + this.$barBtnWidth * (activeNum) + 'px)',
+            });
+        },
+        noti: function(){
+            var name = Array.prototype.slice.call(arguments,0,1)[0];
+            var args = Array.prototype.slice.call(arguments,1);
+            Array.prototype.forEach.call(_self.events[name],function(item){
+                item.cb.apply(item.scope || null, args);
+            });
         }
     }
     method.init();
 }
 
-
+CardSlider.prototype.on = function (name,cb,scope) {
+    if(this.events[name]){
+        this.events[name].push({
+            cb:cb,
+            scope:scope
+        });
+    }
+};
 
 module.exports = CardSlider;
--- pages/js/page/demo.js
+++ pages/js/page/demo.js
@@ -1,9 +1,12 @@
 "use strict";
 require('../../scss/demo.scss');
 $(document).ready(function(){
-    var slider = require('../mods/ui/card-slider');
+    var Slider = require('../mods/ui/card-slider');
     setTimeout(function(){
-        new slider($("#demo"));
+        var slider = new Slider($("#demo"));
+        slider.on('change',function(num){
+            console.log(num);
+        });
     },300);
     console.log('log...........!!!!');
     console.log('hello world!!');
